ec6f07c7618254ee473679c470adf133bc0980f5
samilososami <150162298+Samilososami@users.noreply.github.com>
2025-04-07 13:37:00 +0200

    Update framework.py

--- framework.py
+++ framework.py
@@ -17,18 +17,39 @@ sistema = platform.system()
 arquitectura = platform.architecture()[0]
 
 # variables de estética
-usuario_local = os.environ.get("SUDO_USER", "root")
+usuario = os.getlogin()
+
 
 # variables de prompt
 modules = ""
+modulo_especifico = ""
 
 # variable de bucle principal
 is_running = True
+
+# variables de arquitectura
+clearL = "clear"
+clearW = "cls"
  
 
-def modules_list():
-    print(f"\n{Fore.BLUE}{Style.BRIGHT}[+]{Style.RESET_ALL} Modulos disponibles:\n")
-    print(f"\n{Fore.BLUE}[+]{Style.RESET_ALL} ")
+def list_modules():
+    print("\n╭────────────────────────────────────╮")
+    print("│               Módulos              │")
+    print("╰────────────────────────────────────╯")
+    print("\nMódulos disponibles:")
+    print("  wifi       - Captura de Handshake, Ataques WPS, Desautenticación")
+    print("  bluetooth  - BLE, Bluesnarfing, Enumeración de dispositivos")
+    print("  cracking   - Aircrack-ng, Hashcat, JohnTheRipper\n")
+
+def wifi():
+    global modulo_especifico
+    modulo_especifico = "/wifi"
+
+def bluetooth():
+    global modulo_especifico
+    modulo_especifico = "/bluetooth"
+
+
 
 
 def inicio():
@@ -37,19 +58,22 @@ def inicio():
     print(f"{Fore.YELLOW}[+] Arquitectura:{Fore.BLUE} {arquitectura}{Style.RESET_ALL}\n")
     if sistema != "Linux":
         print(f"{Fore.RED}[!] Este script está diseñado para linux, puede que algunos modulos no funcionen o algunas funciones no se muestren correctamente!{Style.RESET_ALL}\n")
+    elif sistema != "Linux":
+        global clear 
+        clear = "cls"
     else:
          pass
 
 
 def prompt(modules):
-    return input(f'''{Style.BRIGHT}{Fore.GREEN}╭──({Fore.YELLOW}{usuario_local}@framework{Style.RESET_ALL}{Fore.GREEN}{Style.BRIGHT})-[{Style.RESET_ALL}{Fore.MAGENTA}/{modules}{Fore.GREEN}{Style.BRIGHT}]{Style.RESET_ALL}{Fore.GREEN}
+    return input(f'''{Style.BRIGHT}{Fore.GREEN}╭──({Fore.YELLOW}{usuario}@framework{Style.RESET_ALL}{Fore.GREEN}{Style.BRIGHT})-[{Style.RESET_ALL}{Fore.MAGENTA}{Style.BRIGHT}/{modules}{Style.RESET_ALL}{Fore.MAGENTA}{modulo_especifico}{Fore.GREEN}{Style.BRIGHT}]{Style.RESET_ALL}{Fore.GREEN}
 ╰─{Style.BRIGHT}>{Style.RESET_ALL} ''')
 
 
 def instrucciones():
     if modules == "modules":
         print("\n╭────────────────────────────────────╮")
-        print("│            Sección {Fore.MAGENTA}Módulos{Style.RESET_ALL}         │")
+        print(f"│            Sección {Fore.MAGENTA}Módulos{Style.RESET_ALL}         │")
         print("╰────────────────────────────────────╯")
         print("\nComandos disponibles:")
         print("  list        - Listar módulos disponibles")
@@ -65,31 +89,49 @@ def instrucciones():
         print("  modules     - Accede al directorio de módulos")
         print("  x <comando> - Ejecuta un comando a nivel de sistema (ej. 'x whoami')\n")
 
-def modules_wifi():
-
-
-
 
 def comandos(comando):
+    global modules
+    global modulo_especifico
     if comando == "help":
         instrucciones()
     elif comando == "clear":
-        os.system("clear")
+        if sistema == "Linux":
+            os.system(clearL)
+        else:
+            os.system(clearW)
     elif comando == "salir" or comando == "exit": 
         sys.exit(0)
-    elif comando == "modules": 
-        global modules
-        modules = "modules"
-    elif comando == "back" and modules == "modules":
-        modules = ""
     elif comando.startswith("x "):
         cmd = comando[2:]
         os.system(cmd)
     elif comando == "x":
         print(f"{Fore.RED} [!] 'x' necesita un comando a ejecutar (ex. 'x whoami')")
+    elif comando == "modules" and modules == "": 
+        modules = "modules"
+    elif comando == "modules" and modules == "modules":
+        print(f"{Fore.RED}[!] Ya estás en el directorio de módulos!{Style.RESET_ALL}")
+    elif comando == "list" and modules == "modules":
+        list_modules()
+    # retroceder directorio 
+    elif comando == "back" and modulo_especifico != "":
+        modulo_especifico = ""
+    elif comando == "back" and modules == "modules":
+        modules = ""
+    
+    # modulos concretos
+    elif comando.startswith("go ") and modules == "modules":
+        mod = comando[3:].strip().lower()
+        if mod == "wifi":
+            wifi()
+    elif mod == "bluetooth":
+        bluetooth()
 
     else:
         print(f"{Style.BRIGHT}{Fore.RED}[!]{Style.RESET_ALL}{Fore.RED} '{comando}' no es un comando válido, escribe help para listar los comandos disponibles.{Style.RESET_ALL}\n")
+
+
+
 
 try:
     inicio()
@@ -98,4 +140,4 @@ try:
         comandos(user_input)
 
 except KeyboardInterrupt:
-    print(f"\n\n{Fore.RED}[!] hasta la vista, baby{Style.RESET_ALL}")
+    print(f"\n{Fore.RED}[!] hasta la vista, baby{Style.RESET_ALL}")
