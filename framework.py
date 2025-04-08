f6c96a1fd1f1cbac5ca957039115d80dbd0114f6
samilososami <150162298+Samilososami@users.noreply.github.com>
2025-04-08 12:26:20 +0200

    Update framework.py

--- framework.py
+++ framework.py
@@ -36,32 +36,34 @@ clearW = "cls"
 # ----------- FUNCIONES WIFI ------------
 
 def interfaces_inhalambricas():
-    resultado_iwconfig = subprocess.run(["iwconfig"], capture_output=True, text=True)
+    if sistema == "Linux":
+        resultado_iwconfig = subprocess.run(["iwconfig"], capture_output=True, text=True)
     
-    interfaz_actual = None
-    interfaces_modos = []
+        interfaz_actual = None
+        interfaces_modos = []
 
-    for line in resultado_iwconfig.stdout.splitlines():
-        if line and not line.startswith(' '):
-            interfaz_actual = line.split()[0]
+        for line in resultado_iwconfig.stdout.splitlines():
+            if line and not line.startswith(' '):
+                interfaz_actual = line.split()[0]
 
-        elif "Mode:" in line and interfaz_actual:
-            contenido = line.split(":")[1].strip()
-            modo_concreto = contenido.split()[0]
-            interfaces_modos.append((interfaz_actual, modo_concreto))
+            elif "Mode:" in line and interfaz_actual:
+                contenido = line.split(":")[1].strip()
+                modo_concreto = contenido.split()[0]
+                interfaces_modos.append((interfaz_actual, modo_concreto))
 
 
     # si no se encuentra ninguna interfaz:
-    if not interfaces_modos:
-        print(f"\n{Fore.RED}[!] No se han encontrado interfaces inhalambricas!")
+        if not interfaces_modos:
+            print(f"\n{Fore.RED}[!] No se han encontrado interfaces inhalambricas!")
+        else:
+            cantidad = len(interfaces_modos)
+            print(f"{Fore.BLUE}{Style.BRIGHT}[+]{Style.RESET_ALL} {Fore.CYAN}{cantidad}{Style.RESET_ALL} interfaces inhalambricas encontradas:\n")
+
+            for iface, modo in interfaces_modos:
+                print(f"{Fore.GREEN}Interfaz:{Style.RESET_ALL} {iface}")
+                print(f"{Fore.GREEN}Modo:{Style.RESET_ALL} {modo}\n")
     else:
-        cantidad = len(interfaces_modos)
-        print(f"{Fore.BLUE}{Style.BRIGHT}[+]{Style.RESET_ALL} {Fore.CYAN}{cantidad}{Style.RESET_ALL} interfaces inhalambricas encontradas:\n")
-
-        for iface, modo in interfaces_modos:
-            print(f"{Fore.GREEN}Interfaz:{Style.RESET_ALL} {iface}")
-            print(f"{Fore.GREEN}Modo:{Style.RESET_ALL} {modo}\n")
-
+        print(f"{Fore.RED}[!] Windows no es compatible con el módulo wifi!{Style.RESET_ALL}")
 interfaces_inhalambricas()
 
 
@@ -196,7 +198,7 @@ def comandos(comando):
 
 
     else:
-        print(f"{Style.BRIGHT}{Fore.RED}[!]{Style.RESET_ALL}{Fore.RED} '{comando}' no es un comando válido, escribe help para listar los comandos disponibles.{Style.RESET_ALL}\n")
+        print(f"{Style.BRIGHT}{Fore.RED}[!]{Style.RESET_ALL}{Fore.RED} '{comando}' no es un comando válido, escribe {Fore.WHITE}help{Fore.RED} para listar los comandos disponibles.{Style.RESET_ALL}\n")
 
 
 
