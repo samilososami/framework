4f0697c7b1bc2f1e4c02a181faf23b9d01db3b20
samilososami <150162298+Samilososami@users.noreply.github.com>
2025-04-09 12:29:16 +0200

    Update framework.py

--- framework.py
+++ framework.py
@@ -5,12 +5,14 @@ from colorama import Fore, Style, init
 import time 
 import sys
 import subprocess
+from modules.wifi_module import interfaces_inhalambricas
+from modules.bluetooth_module import bluetooth_module
 
 # variables de formateo y colores
 subrayado = '\033[4m'
 morado = '\033[38;5;135m'
 
-
+interfaces_inhalambricas()
 
 # sistema operativo y arquitectura
 sistema = platform.system()
@@ -33,65 +35,6 @@ clearL = "clear"
 clearW = "cls"
  
 
-# ----------- FUNCIONES WIFI ------------
-
-def interfaces_inhalambricas():
-    if sistema == "Linux":
-        resultado_iwconfig = subprocess.run(["iwconfig"], capture_output=True, text=True)
-    
-        interfaz_actual = None
-        interfaces_modos = []
-
-        for line in resultado_iwconfig.stdout.splitlines():
-            if line and not line.startswith(' '):
-                interfaz_actual = line.split()[0]
-
-            elif "Mode:" in line and interfaz_actual:
-                contenido = line.split(":")[1].strip()
-                modo_concreto = contenido.split()[0]
-                interfaces_modos.append((interfaz_actual, modo_concreto))
-
-
-        # si no se encuentra ninguna interfaz:
-        if not interfaces_modos:
-            print(f"\n{Fore.RED}[!] No se han encontrado interfaces inhalambricas!")
-        else:
-            cantidad = len(interfaces_modos)
-            if cantidad > 1:
-                print(f"{Fore.BLUE}{Style.BRIGHT}[+]{Style.RESET_ALL} {Fore.CYAN}{cantidad}{Style.RESET_ALL} interfaces inhalambricas encontradas:\n")
-            else:
-                print(f"{Fore.BLUE}{Style.BRIGHT}[+]{Style.RESET_ALL} {Fore.CYAN}{cantidad}{Style.RESET_ALL} interfaz inhalámbrica encontrada:\n")
-            
-            # cálculo dinámico de las interfaces
-            max_iface_len = max(len(iface) for iface, _ in interfaces_modos)
-            max_mode_len = max(len(modo) for _, modo in interfaces_modos)
-
-            # padding adicional para mejorar visualización
-            iface_col_width = max_iface_len + 10
-            mode_col_width = max_mode_len + 10
-
-            # línea separadora
-            linea = "+" + "-" * iface_col_width + "+" + "-" * mode_col_width + "+"
-
-            # Encabezado
-            print(linea)
-            print(f" {Fore.YELLOW}{'Interfaz'.ljust(iface_col_width)} {'Modo'.ljust(mode_col_width)} {Style.RESET_ALL}")
-            print(linea)
-
-            # filas de datos
-            for iface, modo in interfaces_modos:
-                # Aplicar ljust() y color al modo
-                modo_formateado = modo.ljust(mode_col_width)
-                if modo_formateado == "Managed".ljust(mode_col_width):  # Compara con ljust también
-                    modo_formateado = f"{Fore.MAGENTA}{modo_formateado}{Style.RESET_ALL}"
-                if modo_formateado == "Monitor".ljust(mode_col_width):  # Compara con ljust también
-                    modo_formateado = f"{Fore.GREEN}{Style.BRIGHT}{modo_formateado}{Style.RESET_ALL}"
-    
-                print(f" {Fore.CYAN}{iface.ljust(iface_col_width)}{Style.RESET_ALL} {modo_formateado} ")
-                print(linea)
-    else:
-        print(f"{Fore.RED}[!] Windows no es compatible con el módulo wifi!{Style.RESET_ALL}")
-interfaces_inhalambricas()
 
 
 
