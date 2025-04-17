718d626a2f7f9d73201394bf000eea92366a5591
samilososami <150162298+samilososami@users.noreply.github.com>
2025-04-17 20:33:00 +0200

    Update framework.py

--- framework.py
+++ framework.py
@@ -32,14 +32,29 @@ is_running = True
 # variables de arquitectura
 clearL = "clear"
 clearW = "cls"
+
+# wifi
+
  
 
 
 
 
+# funcion de inicio: banner e información sobre el sistema
+def inicio():
+    print(f"{Fore.GREEN}{Style.BRIGHT}[+]{Style.RESET_ALL} Aquí pondré el banner.\n")
+    print(f"{Fore.YELLOW}[+] Sistema Operativo:{Fore.BLUE} {sistema}{Style.RESET_ALL}")
+    print(f"{Fore.YELLOW}[+] Arquitectura:{Fore.BLUE} {arquitectura}{Style.RESET_ALL}\n")
+    if sistema != "Linux":
+        print(f"{Fore.RED}[!] Este script está diseñado para linux, puede que algunos modulos no funcionen o algunas funciones no se muestren correctamente!{Style.RESET_ALL}\n")
+    elif sistema != "Linux":
+        global clear 
+        clear = "cls"
+    else:
+         pass
 
 
-
+# lista de módulos
 def list_modules():
     print("\n╭────────────────────────────────────╮")
     print("│               Módulos              │")
@@ -65,26 +80,27 @@ def cracking():
 
 
 
-def inicio():
-    print(f"{Fore.GREEN}{Style.BRIGHT}[+]{Style.RESET_ALL} Aquí pondré el banner.\n")
-    print(f"{Fore.YELLOW}[+] Sistema Operativo:{Fore.BLUE} {sistema}{Style.RESET_ALL}")
-    print(f"{Fore.YELLOW}[+] Arquitectura:{Fore.BLUE} {arquitectura}{Style.RESET_ALL}\n")
-    if sistema != "Linux":
-        print(f"{Fore.RED}[!] Este script está diseñado para linux, puede que algunos modulos no funcionen o algunas funciones no se muestren correctamente!{Style.RESET_ALL}\n")
-    elif sistema != "Linux":
-        global clear 
-        clear = "cls"
-    else:
-         pass
-
-
+# función para mostrar el prompt
 def prompt(modules):
     return input(f'''{Style.BRIGHT}{Fore.GREEN}╭──({Fore.YELLOW}{usuario}@framework{Style.RESET_ALL}{Fore.GREEN}{Style.BRIGHT})-[{Style.RESET_ALL}{Fore.MAGENTA}{Style.BRIGHT}/{modules}{Style.RESET_ALL}{Fore.MAGENTA}{modulo_especifico}{Fore.GREEN}{Style.BRIGHT}]{Style.RESET_ALL}{Fore.GREEN}
 ╰─{Style.BRIGHT}>{Style.RESET_ALL} ''')
 
 
+# función para mostrar las instrucciones. el 'help' de toda la vida
 def instrucciones():
-    if modules == "modules":
+    if modulo_especifico == "/wifi" and modules == "modules":
+        print("\n╭────────────────────────────────────╮")
+        print("│               Wifi                 │")
+        print("╰────────────────────────────────────╯")
+        print("\nComandos disponibles:")
+        print("  interfaces/i  - Muestra y elecciona una interfaz compatible")
+        print("  monitor/m    - Activa el modo monitor en la interfaz seleccionada")
+        print("  scan/s     - Escanea las redes cercanas")
+        print("  back      - retrocede al directorio anterior\n")
+
+
+
+    elif modulo_especifico == "" and modules == "modules":
         print("\n╭────────────────────────────────────╮")
         print(f"│            Sección {Fore.MAGENTA}Módulos{Style.RESET_ALL}         │")
         print("╰────────────────────────────────────╯")
@@ -93,14 +109,7 @@ def instrucciones():
         print("  go <módulo> - Accede al módulo seleccionado")
         print("  back        - Volver al menú principal\n")
     
-    elif modulo_especifico == "/wifi":
-        print("\n╭────────────────────────────────────╮")
-        print("│               Wifi                 │")
-        print("╰────────────────────────────────────╯")
-        print("\nComandos disponibles:")
-        print("  interfaz  - Muestra y elecciona una interfaz compatible")
-        print("  monitor   - Activa el modo monitor en la interfaz seleccionada")
-        print("  scan      - Escanea las redes cercanas\n")
+
 
 
 
@@ -118,6 +127,8 @@ def instrucciones():
         print("  x <comando> - Ejecuta un comando a nivel de sistema (ej. 'x whoami')\n")
 
 
+
+# función del bucle principal. se gestionan los comandos del usuario
 def comandos(comando):
     global modules
     global modulo_especifico
@@ -137,6 +148,7 @@ def comandos(comando):
         print(f"{Fore.RED} [!] 'x' necesita un comando a ejecutar (ex. 'x whoami')")
     elif comando == "modules" and modules == "": 
         modules = "modules"
+        instrucciones()
     elif comando == "modules" and modules == "modules":
         print(f"{Fore.RED}[!] Ya estás en el directorio de módulos!{Style.RESET_ALL}")
     elif comando == "list" and modules == "modules":
@@ -152,6 +164,7 @@ def comandos(comando):
         mod = comando[3:].strip().lower()
         if mod == "wifi":
             wifi()
+            instrucciones()
         elif mod == "bluetooth":
             bluetooth()
         elif mod == "cracking":
@@ -162,8 +175,16 @@ def comandos(comando):
     # comandos wifi 
     elif modulo_especifico == "/wifi" and comando == "scan":
         pass
-    elif modulo_especifico == "/wifi" and comando == "interfaces":
-        wifi_module.interfaces()
+    elif modulo_especifico == "/wifi" and comando == "interfaces" or modulo_especifico == "/wifi" and comando == "i":
+        try:
+            wifi_module.interfaces()
+        except KeyboardInterrupt:
+            print()
+            pass
+    elif modulo_especifico == "/wifi" and comando == "monitor" or modulo_especifico == "/wifi" and comando == "m":
+        wifi_module.modo_monitor()
+    elif modulo_especifico == "/wifi" and comando == "scan" or modulo_especifico == "/wifi" and comando == "s":
+        wifi_module.escanear_redes_en_xterm()
     
 
 
